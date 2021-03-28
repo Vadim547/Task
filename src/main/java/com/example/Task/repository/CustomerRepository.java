64616f81e15b6d79e81c17fda4f327cd07edc061
VadimKim <kkimvadim@gmail.com>
2021-03-28 17:03:42 +0500

    findWithout method query in CustomerRepository fixed

--- src/main/java/com/example/Task/repository/CustomerRepository.java
+++ src/main/java/com/example/Task/repository/CustomerRepository.java
@@ -13,9 +13,11 @@ import java.util.Optional;
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
-    @Query("select c2 from Customer c2 left join Order o2 on c2.id = o2.customer.id " +
-            "where o2.date < '2016-01-01' or o2.date > '2016-12-31'" +
-            "group by c2.id")
+    @Query("select c from Customer c where c.id not in(select c2.id " +
+            "FROM  Customer c2" +
+            " left JOIN Order o" +
+            " ON c2.id = o.customer.id " +
+            " where o.date > '2016-01-01' and o.date < '2016-12-31')")
     List<Customer> findWithoutOrder();
 
 
