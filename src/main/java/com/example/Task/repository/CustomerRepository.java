8d552931a024c1e24dcea093c13f3c99a85448eb
VadimKim <kkimvadim@gmail.com>
2021-03-28 12:45:57 +0500

    query of method findWithoutOrder refactored

--- src/main/java/com/example/Task/repository/CustomerRepository.java
+++ src/main/java/com/example/Task/repository/CustomerRepository.java
@@ -13,8 +13,11 @@ import java.util.Optional;
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
-    @Query("select distinct c2 from Customer c2 left join Order o2 on c2.id = o2.customer.id where o2.date < '2016-01-01' or o2.date > '2016-12-31'")
+    @Query("select c2 from Customer c2 left join Order o2 on c2.id = o2.customer.id " +
+            "where o2.date < '2016-01-01' or o2.date > '2016-12-31'" +
+            "group by c2.id")
     List<Customer> findWithoutOrder();
+
 
     @Query("select c2.id, c2.name, max(o2.date) as last_order " +
             "from Customer c2 left join Order o2 on c2.id = o2.customer.id  " +
